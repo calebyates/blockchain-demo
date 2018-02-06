962fe3b777290a97b45c329e2261e0c48618fcdd
calebyates <35265886+calebyates@users.noreply.github.com>
2018-02-06 13:36:38 +0100

    Fixed type error. Validation works.

--- main.js
+++ main.js
@@ -34,27 +34,33 @@ class Blockchain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
-}
 
-isChainValid(){
-    for(let i = 1; 1 < this.chain.length; i++){
-        const currentBlock = this.chain[1];
-        const previousBlock = this.chain[i - 1];
 
-        if(currentBlock.has !== currentBlock.calculateHash()){
-            return false;
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
         }
-        if(currentBlock.previousHash !== previousBlock.hash){
-            return flase;
-        }
+
+        return true;
     }
-
-    return true;
 }
+
+
 
 
 let Z2MCoin = new Blockchain();
 Z2MCoin.addBlock(new Block(1, "02/02/2018", { amount: 4}));
 Z2MCoin.addBlock(new Block(2, "02/03/2018", { amount: 10}));
 
-console.log(JSON.stringify(Z2MCoin, null, 4));
+console.log('is blockchain valid?' + Z2MCoin.isChainValid());
+
+// console.log(JSON.stringify(Z2MCoin, null, 4));
